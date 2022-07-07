c3375c8cd8e9886807b759ce21ef1bb0e7715382
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-07-06 21:11:17 -0300

    Construtor de Personagens

---
+++ 0x00/personagem/personagem-builder/LadraoBuilder.java
@@ -0,0 +1,48 @@
+public class LadraoBuilder implements Builder{
+    String nome;
+    TipoPersonagem tipo;
+    int inteligencia;
+    int forca;
+    int vigor;
+    int resistencia;
+    int destreza;
+
+    @Override
+    public void setNome(String nome) {
+
+    }
+
+    @Override
+    public void setTipo(TipoPersonagem tipo) {
+
+    }
+
+    @Override
+    public void setForca(int forca) {
+
+    }
+
+    @Override
+    public void setInteligencia(int inteligencia) {
+
+    }
+
+    @Override
+    public void setVigor(int vigor) {
+
+    }
+
+    @Override
+    public void setResistencia(int resistencia) {
+
+    }
+
+    @Override
+    public void setDestreza(int destreza) {
+
+    }
+
+    public Ladrao build(){
+        return new Ladrao(nome, inteligencia, forca, vigor, resistencia, destreza);
+    }
+}
