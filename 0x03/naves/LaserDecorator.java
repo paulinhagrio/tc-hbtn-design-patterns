9bc2abee5a255e5e5b516010c1baf44eee46336c
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-07-11 11:08:34 -0300

    Guerra de Naves Espaciais

---
+++ 0x03/naves/LaserDecorator.java
@@ -0,0 +1,12 @@
+public class LaserDecorator extends NaveEspacialDecorator{
+
+
+    public LaserDecorator(NaveEspacial naveDecorada) {
+        super(naveDecorada);
+    }
+
+    @Override
+    public int getAtaque() {
+        return super.getAtaque() + 40;
+    }
+}
---
+++ 0x03/naves/NanoBotsDecorator.java
@@ -0,0 +1,12 @@
+public class NanoBotsDecorator extends NaveEspacialDecorator{
+
+
+    public NanoBotsDecorator(NaveEspacial naveDecorada) {
+        super(naveDecorada);
+    }
+
+    @Override
+    public int getAtaque() {
+        return super.getAtaque() + 25;
+    }
+}
